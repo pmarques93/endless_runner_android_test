7f02b96be7f277339f77cda900baa1f1c98b8fdb
Pedro Marques <pmarques93@gmail.com>
2020-06-18 01:17:50 +0100

    Add water reflection, Add buoyancy effector

---
+++ Game/Assets/Resources/Water/WaterBuoyancy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBuoyancy : MonoBehaviour
+{
+
+    // 0.6 0.95
+
+    // Inspector stuff
+    [SerializeField] BuoyancyEffector2D effector;
+
+    // Density
+    private bool goingUp;
+
+    private void Start()
+    {
+        goingUp = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (goingUp)
+        {
+            effector.density += 0.5f * Time.fixedDeltaTime;
+            if (effector.density >= 0.95) goingUp = false;
+        }
+
+        if (goingUp == false)
+        {
+            effector.density -= 0.5f * Time.fixedDeltaTime;
+            if (effector.density <= 0.60) goingUp = true;
+        }
+    }
+}
